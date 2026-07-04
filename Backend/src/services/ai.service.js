5ea091fa20798f32480cad389c75859c474c1024
Chirag Greed <chiraggreed02@gmail.com>
2026-07-04 21:21:58 +0530

    fix: Updated langchain to latest version

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,15 +1,26 @@
 import { HumanMessage } from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai"
 
-const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash-lite",
-    apiKey: process.env.GOOGLE_API_KEY
+const Geminimodel = async (query) => {
+    const response = await model.invoke(query);
+    return (response.text);
+}
+
+const Mistralmodel = new ChatMistralAI({
+    model: "mistral-small-2603",
+    apiKey: process.env.MISTRAL_API_KEY
 });
 
 export const invokeAi = async ({ query }) => {
-    const response = await model.invoke([
+    const response = await Geminimodel.invoke([
         new HumanMessage(query)]);
 
     return (response.text);
 
+}
+
+export const generateChatTitle = async ({ title }) => {
+    const response = await Mistralmodel.invoke(`Create a chat summary title under 10 words for this query: '${title}'`);
+    return (response.text);
 }
